a0a41fd8ee9f33fe03f7a4fdf391fb27a4f3f2f6
Josuu <jmzo17.jz@gmail.com>
2025-11-19 21:55:07 +0100

    Diseño Basico

--- app/src/main/java/com/josuu1/calculadora/MainActivity.java
+++ app/src/main/java/com/josuu1/calculadora/MainActivity.java
@@ -1,6 +1,8 @@
 package com.josuu1.calculadora;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button0;
+
+        button1 = findViewById(R.id.ButtonOne);
+        button2 = findViewById(R.id.ButtonTwo);
+        button3 = findViewById(R.id.ButtonThree);
+        button4 = findViewById(R.id.ButtonFour);
+        button5 = findViewById(R.id.ButtonFive);
+        button6 = findViewById(R.id.ButtonSix);
+        button7 = findViewById(R.id.ButtonSeven);
+        button8 = findViewById(R.id.ButtonEight);
+        button9 = findViewById(R.id.ButtonNine);
+        button0 = findViewById(R.id.ButtonCero);
+
+        //Button.OnClickListener(new View.OnClickListener(){
+
+        };
     }
-}
+
